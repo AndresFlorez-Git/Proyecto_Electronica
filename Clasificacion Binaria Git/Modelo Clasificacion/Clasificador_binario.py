65a02211221a2ed5c6b862118d2d3f3bc2c618e3
unknown <af.florez12@uniandes.edu.co>
2020-08-30 21:12:42 -0500

    Modelo de clasificacion completo/ matriz confusion

--- Clasificacion Binaria Git/Modelo Clasificacion/Clasificador_binario.py
+++ Clasificacion Binaria Git/Modelo Clasificacion/Clasificador_binario.py
@@ -17,7 +17,9 @@ from skimage.io import imread, imshow
 from skimage.transform import resize
 from tqdm import tqdm
 import random
-
+from keras.utils import plot_model
+from sklearn.metrics import confusion_matrix
+import itertools
 ##############################################################################
 ######################  Declaración de variables #############################
 ##############################################################################
@@ -32,11 +34,24 @@ IMG_HEIGHT = 200
 IMG_CHANNELS = 3
 
 ##############################################################################
+############################  Path Base  #####################################
+##############################################################################
+
+BASE_PATH = str(os.path.dirname(os.path.abspath(''))) # path del la carpeta Segmentacion Semantica Git
+
+if ('\\' in BASE_PATH) == True:
+    separator_dir = '\\'
+else:
+    separator_dir = '/'
+
+
+
+##############################################################################
 ####################  Path del data set original #############################
 ##############################################################################
 
-TRAIN_PATH_IMAGES = 'Dataset\Segmentar\Entreno\ImaEscalada'
-TRAIN_PATH_MASKS = 'Dataset\Segmentar\Entreno\LabelsFinales'
+TRAIN_PATH_IMAGES = BASE_PATH + separator_dir + 'Data Set' + separator_dir + 'Mezcla' + separator_dir + 'Con ajuste' + separator_dir + 'Images'
+TRAIN_PATH_MASKS = BASE_PATH + separator_dir + 'Data Set' + separator_dir + 'Mezcla' + separator_dir + 'Con ajuste' + separator_dir + 'Masks'
 
 
 ##############################################################################
@@ -44,9 +59,9 @@ TRAIN_PATH_MASKS = 'Dataset\Segmentar\Entreno\LabelsFinales'
 ##############################################################################
 
 # Path de las imagenes, mascaras e imagenes de prueba
-# TRAIN_PATH_IMAGES = 'Augmentation_data_set_segmentation\images'
-# TRAIN_PATH_MASKS = 'Augmentation_data_set_segmentation\masks'
-TEST_PATH_IMAGES = 'Dataset\Segmentar\Test'
+# TRAIN_PATH_IMAGES = BASE_PATH + separator_dir + 'Augmented Train Data' + separator_dir + 'Images'
+# TRAIN_PATH_MASKS = BASE_PATH + separator_dir + 'Augmented Train Data' + separator_dir + 'Masks'
+
 
 
 ##############################################################################
@@ -56,13 +71,13 @@ TEST_PATH_IMAGES = 'Dataset\Segmentar\Test'
 # Se obtienen los Ids de las imagenes aumentadas.
 Train_images_files = next(os.walk(TRAIN_PATH_IMAGES))[2]
 Train_masks_files = next(os.walk(TRAIN_PATH_MASKS))[2]
-Test_images_files = next(os.walk(TEST_PATH_IMAGES))[2]
+
 
 
 # Se declaran los arrays que almacenan las imagenes
 X_train = np.zeros((len(Train_images_files), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
-Y_train = np.zeros((len(Train_images_files), IMG_HEIGHT*IMG_WIDTH), dtype=np.bool)
-X_test = np.zeros((len(Test_images_files), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+Y_train = np.zeros((len(Train_images_files), IMG_HEIGHT, IMG_WIDTH,1), dtype=np.bool)
+Y_train_class = np.zeros((len(Train_images_files), 1), dtype=np.bool)
 
 
 # Se inicia el proceso de resizing de las imagenes de entrenamiento y las de prueba
@@ -72,28 +87,38 @@ for n, id_ in tqdm(enumerate(Train_images_files), total=len(Train_images_files))
     path_image = TRAIN_PATH_IMAGES + '\\'
     path_mask = TRAIN_PATH_MASKS + '\\'
     img = imread(path_image + Train_images_files[n])[:,:,:IMG_CHANNELS]
-    # img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
     X_train[n] = img
+    
     mask = imread(path_mask + Train_masks_files[n])
     mask = np.expand_dims(mask, axis=-1)
-    mask = mask.ravel()
     Y_train[n] = mask
- 
+    X_train[n] = X_train[n]*Y_train[n]
+    
+    name = id_[:5]
+    if name == 'N_C_F':
+        Y_train_class[n] = 1
+    if name == 'N_C_S':
+        Y_train_class[n] = 1
+    if name == 'N_C_T':
+        Y_train_class[n] = 1
+    if name == 'N_S_S':
+        Y_train_class[n] = 0
+    
+
+
+
+
+
 # Es necesario organizar de forma aleatoria el dataset aumentado para un correcto entrenamiento del modelo
 index = np.arange(X_train.shape[0])
 np.random.shuffle(index)
 
-X_train = X_train[index]
-Y_train = Y_train[index]
 
-print('Resizing test images')
-for n, id_ in tqdm(enumerate(Test_images_files), total=len(Test_images_files)):
-    path_image = TEST_PATH_IMAGES + '\\'
-    img = imread(path_image + Test_images_files[n])[:,:,:IMG_CHANNELS]
-    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
-    X_test[n] = img
-    
-    
+X_train = X_train[index]
+Y_train_class = Y_train_class[index]
+
+
+
 ##############################################################################
 #####################  Arquitectura del modelo ###############################
 ##############################################################################
@@ -115,11 +140,11 @@ model.add(Conv2D(256, (5,5),  activation='relu', padding='same'))
 model.add(BatchNormalization(axis = 1))
 model.add(MaxPooling2D((2,2)))
 model.add(Flatten())
-model.add(Dense(2,  activation='sigmoid'))
+model.add(Dense(1,  activation='sigmoid'))
 
 
 # Salida
-model.compile(optimizer='adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+model.compile(optimizer='rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
 model.summary()
 
 
@@ -131,21 +156,22 @@ model.summary()
 
 checkpointer = tf.keras.callbacks.ModelCheckpoint('model_for_pv.h5', verbose=1, save_best_only = True)
 
-callbacks = [tf.keras.callbacks.EarlyStopping(patience=4, monitor='val_loss'),
+callbacks = [tf.keras.callbacks.EarlyStopping(patience=6, monitor='val_loss'),
               tf.keras.callbacks.TensorBoard(log_dir='logs')]
 
 ##############################################################################
 ####################  Resultados del modelo ##################################
 ##############################################################################
-results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=10, epochs=50, callbacks=callbacks)
+results = model.fit(X_train, Y_train_class, validation_split=0.1, batch_size=10, epochs=20, callbacks=callbacks)
 
 # Descomentar si se quiere guardar el modelo entrenado
 Val_acc = results.history['val_acc'][-1]
-model.save('Modelos_Guardados\Modelo_Basic_val_acc_'+str(round(Val_acc,4))+'.h5')
+model.save(BASE_PATH + separator_dir + 'Modelos_Guardados' + separator_dir + 'Modelo_Clasificacion_val_acc_'+str(round(Val_acc,4))+'.h5')
 
 plt.figure(1,figsize=(5,5))
 plt.plot(results.history['loss'], label = 'Train')
 plt.plot(results.history['val_loss'], label = 'Validation loss')
+# plt.axis([0,20,0,1])
 plt.legend()
 plt.show()
 
@@ -153,6 +179,7 @@ plt.figure(2,figsize=(5,5))
 plt.plot(results.history['acc'], label = 'Train acc')
 plt.plot(results.history['val_acc'], label = 'Validation acc')
 plt.legend()
+# plt.axis([0,20,0,1])
 plt.show()
 
 ##############################################################################
@@ -160,41 +187,67 @@ plt.show()
 ##############################################################################
 
 preds_train = model.predict(X_train, verbose=1)
-preds_test = model.predict(X_test, verbose=1)
-
-Y_train2 = np.zeros((len(Train_images_files), IMG_HEIGHT, IMG_WIDTH), dtype=np.bool)
-preds_train2 = np.zeros((len(Train_images_files), IMG_HEIGHT, IMG_WIDTH), dtype=np.bool)
-preds_test2 =np.zeros((len(Test_images_files), IMG_HEIGHT, IMG_WIDTH), dtype=np.bool)
+preds_train = (preds_train>0.5).astype(np.uint8)
 
 
-for n, id_ in tqdm(enumerate(Train_images_files), total=len(Train_images_files)):
-    Y_train2[n] = np.reshape(Y_train[n],(200,200))
-    preds_train2[n] = np.reshape(preds_train[n],(200,200))
-
-for n, id_ in tqdm(enumerate(Test_images_files), total=len(Test_images_files)):
-    preds_test2[n] = np.reshape(preds_test[n],(200,200))
-    
-preds_train_t = (preds_train2 > 0.9).astype(np.uint8)
-preds_test_t = (preds_test2 > 0.9).astype(np.uint8)
-
-
-# Perform a sanity check on some random training samples
-ix = random.randint(0, len(preds_train_t)-1)
+# # Perform a sanity check on some random training samples
+ix = random.randint(0, len(preds_train)-1)
 imshow(X_train[ix])
 plt.show()
-imshow(np.squeeze(Y_train2[ix]))
-plt.show()
-imshow(np.squeeze(preds_train_t[ix]))
-plt.show()
+
+print('Predicción: ', preds_train[ix])
+print('Real: ', Y_train_class[ix])
+# imshow(np.squeeze(Y_train2[ix]))
+# plt.show()
+# imshow(np.squeeze(preds_train_t[ix]))
+# plt.show()
 
 
 
-# Perform a sanity check on some random test samples
-ix = random.randint(0, len(preds_test_t)-1)
-imshow(X_test[ix])
-plt.show()
-imshow(np.squeeze(preds_test_t[ix]))
-plt.show()
 
+
+##############################################################################
+##############  Función para visualizar matriz de confusión ##################
+##############################################################################
+# Esta función se encuentra dentro d los métodos de sklearn
+
+def plot_confusion_matrix(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+
+##############################################################################
+########################  Matriz de confusión ################################
+##############################################################################
 
 
